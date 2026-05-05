f4092d14d701fcd0d6f58bfae553ae4fd476063f
Bhewitt98 <bhewitt20@ivytech.edu>
2026-05-05 19:57:15 -0400

    Attempt #2

--- team_grey_-_ticket_to_ride_v0.1.4/team-grey---ticket-to-ride/UI/start_menu.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-@onready var menu_music = $MenuMusic
-
-func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://UI/main.tscn")
-	
-func _on_quit_button_pressed():
-	get_tree().quit()
